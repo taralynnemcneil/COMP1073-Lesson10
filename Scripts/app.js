e4d3fa63ff3962a4445cf2ea78fdafb03a46c09e
Tara McNeil <taralynnemcneil@gmail.com>
2016-03-31 13:20:25 -0400

    Using Jquery

--- Scripts/app.js
+++ Scripts/app.js
@@ -6,11 +6,27 @@
     "use strict";
 
     var mainContentH1 = $('#mainContent h1')[0];
+    
+    var welcome = $('#welcome').text();
 
     var mainContentH1JS = document.querySelectorAll('#mainContent h1')[1];
     
-    console.log(mainContentH1);
-    console.log(mainContentH1JS);
+    //console.log(mainContentH1.textContent);
+    //console.log(mainContentH1JS);
+    //console.log(welcome);
 
+    /* ---- this is the way of doing it with pure vanilla JS
+    
+    var form1JS = document.getElementById('form1');
+    form1JS.style.width = "50%";
+    ------------------------------------------------------ */
+    
+    // this is the same way to do it but shorter using JQUERY
+    $('#form1').css('width', '50%');
+    
+    // targeting specific attributes (in this case the Placeholder)
+    $('#form1 input').attr('placeholder', 'Enter your username');
+    
+    
 })();
 
